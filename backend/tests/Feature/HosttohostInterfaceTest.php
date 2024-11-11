d7c0d3d256ec7bf3c8277ec80b9e7f499176f109
maulana saputra <maulana.saputra@aqi.co.id>
2024-11-12 00:39:03 +0700

    feat: interface client
    Signed-off-by: maulana saputra <maulana.saputra@aqi.co.id>

--- backend/tests/Feature/HosttohostInterfaceTest.php
+++ backend/tests/Feature/HosttohostInterfaceTest.php
@@ -4,17 +4,20 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class HosttohostInterfaceTest extends TestCase
 {
     use DatabaseTransactions;
 
-    public function test_check_client_with_service(): void
+    public function test_check_client_session_with_local_storage(): void
     {
         $service   = new \Tests\TestClasses\HandleAirAsiaService;
         $interface = \App\Models\Itf::factory()->create();
         $service->start($interface);
-        $this->assertSame("interface.dat", "interface.dat");
+        $pathFile  = Storage::disk("public")->path("interface/airasia/interface-{$interface->id}.dat");
+        $this->assertTrue(file_exists($pathFile));
+        $service->stop($interface);
     }
 }
